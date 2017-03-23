e28e5eb5f530c2e82ed4eb5a8e22c9fc08938bfc
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-03-23 11:00:18 +0200

    Added compnay dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,5 +7,12 @@ class DashboardsController < ApplicationController
 
   def company
     @company = current_user.company
+    # @requests = Request.all
+    # @insuranceservices = current_user.company.insuraceservices
+    # if request && request.insurance_service.company.current_user
+    #   request = Request.find_by(id: params[:id])
+    # else
+    #   #do nothing
+    # end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], is_company: params[:is_company])
     if user.save
       session[:user_id] = user.id
       flash[:success] = 'Successfully created account!'
-      redirect_to "/dashboard"
+      if user.is_company
+        redirect_to "/companies/new"
+      else
+        redirect_to "/client_dashboard"
+      end
     else
       flash[:warning] = 'Invalid email or password!'
       redirect_to "/signup"
